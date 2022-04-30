3e5bf9c19c2ba46dabeae828bb05f362296cf4d3
Nicollas <nicksz55@hotmail.com>
2022-04-30 11:03:11 -0300

    Aprendendo o comando while

---
+++ while.js
@@ -0,0 +1,19 @@
+/*let x = 1;
+
+while(x <= 10){
+    
+    x++;
+    if(x == 5){
+        continue;
+    }
+    document.write(x + '<br />');
+}*/
+
+let x = 1;
+
+do {
+
+    document.write(x + '<br />');
+    x += 2;
+
+} while(x <= 10);
