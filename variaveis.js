1727a9f766a749a6e3706571eba7709ee1efca66
MartinLooterKing <nicksz55@hotmail.com>
2022-04-30 18:07:35 -0300

    Aprendendo sobre variaveis

---
+++ variaveis.js
@@ -0,0 +1,12 @@
+const name = 'Nicollas ';
+        const name2 = prompt('digite seu sobrenome: ');
+       /* const fractionNumber = 7.5;
+        const number = 8;
+        const condicion = true;
+
+       // alert 
+       document.write(name);
+       console.log(number); */
+
+       document.write(name + name2);
+       document.write('<br /><br />');
