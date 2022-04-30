cf0c295faa3b8d2bcc391604d3d9774522345c71
MartinLooterKing <nicksz55@hotmail.com>
2022-04-30 18:07:23 -0300

    Aprendendo sobre function

---
+++ function.js
@@ -0,0 +1,21 @@
+/*function iniciarAula(nomeAluno){
+    return 'Olá, ' + nomeAluno + '! A aula começou.';
+}
+
+const mensagem = iniciarAula('Nicollas');
+
+console.log(mensagem);*/
+
+/*const iniciarAula = function (nomeAluno){
+    return 'Olá, ' + nomeAluno + '! A aula começou.';
+}
+
+const mensagem = iniciarAula('Nicollas');
+
+console.log(mensagem);*/
+
+const iniciarAula = (nomeAluno) => `Olá, ${nomeAluno}! A aula começou.`;
+
+const mensagem = iniciarAula('Nicollas');
+
+console.log(mensagem);
