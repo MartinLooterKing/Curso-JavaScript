0bc5e7386c95c5d9ca1f910a92050b9709765f74
MartinLooterKing <nicksz55@hotmail.com>
2022-05-01 10:47:50 -0300

    Ajeitando o erro

--- variaveis.js
+++ variaveis.js
@@ -1,4 +1,4 @@
-const name = 'Nicollas ';
+const names = 'Nicollas ';
         const name2 = prompt('digite seu sobrenome: ');
        /* const fractionNumber = 7.5;
         const number = 8;
@@ -8,5 +8,5 @@ const name = 'Nicollas ';
        document.write(name);
        console.log(number); */
 
-       document.write(name + name2);
+       document.write(names + name2);
        document.write('<br /><br />');
