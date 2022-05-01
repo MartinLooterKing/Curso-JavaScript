82773a70034e8be840127600c34097f3fe83f292
MartinLooterKing <nicksz55@hotmail.com>
2022-05-01 10:48:40 -0300

    Novas interações  para o for

--- for.js
+++ for.js
@@ -1,6 +1,18 @@
-for(let x = 1; x < 11; x++){
+/*for(let x = 1; x < 11; x++){
     document.write(x + '<br />');
 
     //break;
     //continue;
+}*/
+
+const pessoas = [{ nome: 'Nicollas', idade: 23 }, 
+{ nome: 'Marylya', idade: 24 }
+];
+
+const lista = pessoas.length;
+
+for(let index = 0; index < lista; index++){
+    const nome = pessoas[index].nome;
+    const idade = pessoas[index].idade;
+    console.log(nome, idade);
 }
