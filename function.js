e9347fef274a50dd6a826c3aaead83b18d281776
MartinLooterKing <nicksz55@hotmail.com>
2022-05-01 22:54:57 -0300

    Aprendendo mais um pouco sobre funções

--- function.js
+++ function.js
@@ -14,8 +14,24 @@ const mensagem = iniciarAula('Nicollas');
 
 console.log(mensagem);*/
 
-const iniciarAula = (nomeAluno) => `Olá, ${nomeAluno}! A aula começou.`;
+/*const iniciarAula = (nomeAluno) => `Olá, ${nomeAluno}! A aula começou.`;
 
 const mensagem = iniciarAula('Nicollas');
 
-console.log(mensagem);
+console.log(mensagem);*/
+
+function calculoAreaTerreno (largura, comprimento){
+
+    largura = parseFloat(largura);
+    comprimento = parseFloat(comprimento);
+    
+    let area = largura * comprimento;
+    return area;
+
+}
+
+let largura = prompt('Digite a largura do terreno');
+let comprimento = prompt('Digite o comprimento do terreno');
+
+//let areaTerreno = calculoAreaTerreno(15, 25);
+document.write(`O terreno possui ${calculoAreaTerreno(largura, comprimento)} metros quadrados.`);
