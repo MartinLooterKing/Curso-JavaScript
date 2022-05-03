7db0480ba55fe450fd8244af846767b848487df5
Nicollas <nicksz55@hotmail.com>
2022-05-03 14:03:37 -0300

    Aprendendo um pouco sobre eventos

---
+++ eventos.js
@@ -0,0 +1,11 @@
+//mouse - onclick, ondblclick, onmousedown, onmouseover, onmouseout
+
+// function msgBoasVindas(){
+//     alert('Seja bem-vindo, você pressionou o botão esquerdo do mouse sobre este elemento');
+// }
+
+//teclado - onkeydown, onkeypress, onkeyup
+
+// janelas - onload, onresize
+
+//formulários - onfocus, onblur, onchange
