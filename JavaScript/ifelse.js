f0e24778c22ad93592f45ec7f6ceb3b16a92a634
Nicollas <nicksz55@hotmail.com>
2022-05-06 14:41:46 -0300

    Movidos para a pasta JavaScript

---
+++ JavaScript/ifelse.js
@@ -0,0 +1,20 @@
+//==, ===, != ou <>, !==, <, >, <=, >=
+
+/*const nota = prompt('Digite a nota do aluno');
+const media = 7;
+
+if(nota >= media){
+    document.write('Aprovado');
+} else {
+    document.write('Reprovado');
+}*/
+
+// && - True se todas forem True 
+// || - Precisa somente uma ser True
+// ! - Negação, inverte o valor - se for true inverte para false
+
+if(!(2>3)){
+    document.write('Verdadeiro');
+} else {
+    document.write('Falso');
+}
